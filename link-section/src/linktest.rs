b7caeea14d198e748673864186b6936572e98f60
Matt Mastracci <matthew@mastracci.com>
2026-04-29 17:18:28 -0600

    .

---
+++ link-section/src/linktest.rs
@@ -0,0 +1,14 @@
+#[link_section = ".text.data.1"]
+pub static mut DATA1: u8 = 1;
+#[link_section = ".text.data.2"]
+pub static mut DATA2: u8 = 1;
+#[link_section = ".text.data.3"]
+pub static mut DATA3: u8 = 1;
+
+pub fn main() {
+    unsafe {
+        println!("DATA1: {:?}", DATA1);
+        println!("DATA2: {:?}", DATA2);
+        println!("DATA3: {:?}", DATA3);
+    }
+}
