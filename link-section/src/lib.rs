418f766ff2d7fa923b51bcba513b07837876e84a
Matt Mastracci <matthew@mastracci.com>
2026-04-29 19:34:23 -0600

    .

--- link-section/src/lib.rs
+++ link-section/src/lib.rs
@@ -434,7 +434,7 @@ pub mod __support {
                     $crate::__support::add_section_link_attribute!(
                         data section $ident $($aux)?
                         #[link_section = __]
-                        #[export_name = concat!("__", stringify!($ident), $(stringify!($aux),)? "_end")]
+                        #[export_name = concat!("__", stringify!($ident), $(stringify!($aux),)? "_ref")]
                         #[no_mangle]
                         #[used]
                         static mut __REFERENCE:  ::core::mem::MaybeUninit<[$generic_ty; 0]> = ::core::mem::MaybeUninit::uninit();
