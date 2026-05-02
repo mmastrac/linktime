ab8e2cde81ed5b0f71d6d61a1268756a29cf1b62
Matt Mastracci <matthew@mastracci.com>
2026-05-02 12:46:48 -0600

    .

--- link-section/src/lib.rs
+++ link-section/src/lib.rs
@@ -2,6 +2,7 @@
 //! # link-section
 #![doc = include_str!("../docs/PREAMBLE.md")]
 #![allow(unsafe_code)]
+#![cfg_attr(linktime_used_linker, doc(test(attr(feature(used_with_arg)))))]
 
 #[doc = include_str!("../docs/LIFE_BEFORE_MAIN.md")]
 pub mod life_before_main {}
