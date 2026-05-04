fa1712b88823ccb9109dc81b6416b105052a0411
Matt Mastracci <matthew@mastracci.com>
2026-05-03 21:12:55 -0600

    use

--- dtor/tests/macrotest.rs
+++ dtor/tests/macrotest.rs
@@ -80,6 +80,7 @@ pub fn trybuild() {
     t.pass("tests/pass/*.rs");
 }
 
+#[cfg(not(linktime_used_linker))] // we don't want to change tests for this
 #[test]
 pub fn target_test() {
     let Some(toolchain) = std::env::var_os("TOOLCHAIN") else {
