e8bcf2f50e1f8448c8b4fefeaad435e62494d80a
Matt Mastracci <matthew@mastracci.com>
2026-05-05 14:29:58 -0600

    Improve example

--- link-section/examples/const.rs
+++ link-section/examples/const.rs
@@ -5,11 +5,11 @@ use link_section::{in_section, section};
 
 struct Driver {
     name: &'static str,
-    f: fn (),
+    f: fn(),
 }
 
 impl Driver {
-    pub const fn new(name: &'static str, f: fn ()) -> Self {
+    pub const fn new(name: &'static str, f: fn()) -> Self {
         Self { name, f }
     }
 }
@@ -18,17 +18,32 @@ impl Driver {
 static DATA_SECTION: link_section::TypedSection<Driver>;
 
 #[in_section(DATA_SECTION)]
-pub const POSTGRES_DRIVER: Driver = Driver::new("postgres", || println!("postgres"));
+pub const POSTGRES_DRIVER: Driver = Driver::new("postgres", || println!("connected to postgres!"));
 
 #[in_section(DATA_SECTION)]
-pub const MYSQL_DRIVER: Driver = Driver::new("mysql", || println!("mysql"));
+pub const MYSQL_DRIVER: Driver = Driver::new("mysql", || println!("connected to mysql!"));
 
 #[in_section(DATA_SECTION)]
-pub const SQLITE_DRIVER: Driver = Driver::new("sqlite", || println!("sqlite"));
+pub const SQLITE_DRIVER: Driver = Driver::new("sqlite", || println!("connected to sqlite!"));
+
+#[section]
+static DATABASES: link_section::TypedSection<(&'static str, &'static Driver)>;
+
+#[in_section(DATABASES)]
+pub const POSTGRES_DATABASE: (&'static str, &'static Driver) =
+    ("postgres://localhost:5432", &POSTGRES_DRIVER);
+
+#[in_section(DATABASES)]
+pub const MYSQL_DATABASE: (&'static str, &'static Driver) =
+    ("mysql://localhost:3306", &MYSQL_DRIVER);
+
+#[in_section(DATABASES)]
+pub const SQLITE_DATABASE: (&'static str, &'static Driver) =
+    ("sqlite://localhost:1433", &SQLITE_DRIVER);
 
 fn main() {
-    for driver in DATA_SECTION {
-        println!("Connecting to {}...", driver.name);
+    for (url, driver) in DATABASES {
+        println!("Connecting to {url} ({})...", driver.name);
         (driver.f)();
     }
 }
--- link-section/examples/example.rs
+++ link-section/examples/example.rs
@@ -67,9 +67,8 @@ pub static DEBUGGABLE_2: &'static (dyn ::core::fmt::Debug + Sync) = &2;
 
 /// A function pointer in the `DEBUGGABLES` section.
 #[in_section(DEBUGGABLES)]
-pub static DEBUGGABLE_FUNCTION: &'static (dyn ::core::fmt::Debug + Sync) = {
-    &::core::fmt::from_fn(|f| f.write_str("debuggable_function"))
-};
+pub static DEBUGGABLE_FUNCTION: &'static (dyn ::core::fmt::Debug + Sync) =
+    { &::core::fmt::from_fn(|f| f.write_str("debuggable_function")) };
 
 /// Dumps the various sections.
 pub fn main() {
