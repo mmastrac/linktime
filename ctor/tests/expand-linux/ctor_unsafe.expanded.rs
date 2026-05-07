c5a491ad84ba52368e846d7e51dfaff79d61403b
Matt Mastracci <matthew@mastracci.com>
2026-05-07 06:40:22 -0600

    linux expansions, test priority on bsds

--- ctor/tests/expand-linux/ctor_unsafe.expanded.rs
+++ ctor/tests/expand-linux/ctor_unsafe.expanded.rs
@@ -1,6 +1,26 @@
 use ctor::ctor;
+#[allow(dead_code)]
 unsafe fn foo() {
-    {
-        ::std::io::_print(format_args!("foo\n"));
+    #[allow(unsafe_code)]
+    #[link_section = ".text.startup"]
+    unsafe fn __ctor_private_inner() {
+        {
+            ::std::io::_print(format_args!("foo\n"));
+        };
+    }
+    const _: () = {
+        #[allow(unsafe_code)]
+        #[link_section = ".init_array.500"]
+        #[used]
+        static __CTOR_PRIVATE_REF: unsafe extern "C" fn() = {
+            #[allow(unused_unsafe)]
+            #[allow(unsafe_code)]
+            #[link_section = ".text.startup"]
+            extern "C" fn __ctor_private() {
+                { unsafe { __ctor_private_inner() } }
+            }
+            __ctor_private
+        };
     };
+    unsafe { __ctor_private_inner() }
 }
