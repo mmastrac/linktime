6239f6272db14fe3cceb79be228251eae2b54f15
Matt Mastracci <matthew@mastracci.com>
2026-05-07 06:33:13 -0600

    Regenerate AIX and docs

--- ctor/tests/target-test/ctor_priority/powerpc64-ibm-aix.rs
+++ ctor/tests/target-test/ctor_priority/powerpc64-ibm-aix.rs
@@ -12,7 +12,7 @@ fn early() {
             #[allow(unused_unsafe, unsafe_code)]
             #[no_mangle]
             #[export_name =
-            "__sinit80000000_expand_probe_expand_probe_early_L5C1"]
+            "__sinit80000101_expand_probe_expand_probe_early_L5C1"]
             extern "C" fn __ctor_private() { { { __ctor_private_inner() } } }
         };
     { __ctor_private_inner() }
