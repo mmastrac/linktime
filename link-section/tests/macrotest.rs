5b056781dc915d1bffb91360f2ac954aa911d27d
Matt Mastracci <matthew@mastracci.com>
2026-05-12 16:52:52 -0600

    Remove rustflags before target test

--- link-section/tests/macrotest.rs
+++ link-section/tests/macrotest.rs
@@ -172,6 +172,7 @@ link-section = {{ path = "{repo_root}/link-section", default-features = false }}
                 .current_dir(dir.path())
                 .env("CARGO_TERM_COLOR", "never")
                 .env("CARGO_TARGET_DIR", target_dir.join(&target))
+                .env_remove("RUSTFLAGS")
                 .args([
                     "+nightly",
                     "rustc",
