7c55da9bd29e73561cc0f3f9e15b150852259429
Matt Mastracci <matthew@mastracci.com>
2026-06-23 16:45:58 -0600

    blackbox the pointers on windows

--- link-section/src/platform/mod.rs
+++ link-section/src/platform/mod.rs
@@ -37,8 +37,14 @@ pub const fn validate_section_name(name: &str) {
 }
 
 /// Launder a pointer's provenance so it appears as an "exposed" pointer.
+#[cfg(not(windows))]
 pub fn launder_pointer_provenance<T>(ptr: *const T) -> *const T {
     core::ptr::with_exposed_provenance(ptr.expose_provenance())
+}
+
+#[cfg(windows)]
+pub fn launder_pointer_provenance<T>(ptr: *const T) -> *const T {
+    core::hint::black_box(core::ptr::with_exposed_provenance(ptr.expose_provenance()))
 }
 
 /// Constant bounds for a pointer-based section.
