fe02bfa1f209f92f07f6a546c5438f274554b7a8
Matt Mastracci <matthew@mastracci.com>
2026-07-15 18:39:50 -0600

    global_asm fix

--- ctor/src/lib.rs
+++ ctor/src/lib.rs
@@ -45,6 +45,7 @@ crate::__ctor_parse_internal!(
     /// <https://github.com/rust-lang/rust/issues/52234>)
     #[ctor(unsafe, naked)]
     #[allow(unsafe_code)]
+    #[doc(hidden)]
     fn priority_ctor() {
         unsafe {
             crate::collect::run_constructors();
@@ -154,6 +155,39 @@ pub mod collect {
         pub static GUARD_ATOMIC: AtomicU8 = AtomicU8::new(GUARD_NOT_RUN);
     );
 
+    // TODO: We should allow explicit export_name on the ctor itself.
+    #[cfg(all(feature = "priority", target_vendor = "apple"))]
+    #[used]
+    #[doc(hidden)]
+    #[export_name = concat!(
+        "ctor_ap_",
+        env!("CARGO_PKG_VERSION_MAJOR"),
+        "_",
+        env!("CARGO_PKG_VERSION_MINOR"),
+        "_",
+        env!("CARGO_PKG_VERSION_PATCH")
+    )]
+    pub static APPLE_PRIORITY_ANCHOR: fn() = crate::priority_ctor;
+
+    #[macro_export]
+    #[doc(hidden)]
+    macro_rules! __keep_alive {
+        () => {
+            /// Force `ld64` to pull the archive member owning `APPLE_PRIORITY_ANCHOR`
+            /// (see https://github.com/mmastrac/linktime/issues/496).
+            const _: () = {
+                mod __ctor_force {
+                    ::core::arch::global_asm!(
+                        ".pushsection __DATA,__ctor_force,regular,no_dead_strip\n",
+                        ".quad {0}\n",
+                        ".popsection\n",
+                        sym $crate::collect::APPLE_PRIORITY_ANCHOR,
+                    );
+                }
+            };
+        };
+    }
+
     #[macro_export]
     #[doc(hidden)]
     macro_rules! __register_ctor {
@@ -167,6 +201,7 @@ pub mod collect {
             $crate::__register_ctor!(priority = ($crate::collect::LATE), fn = $fn);
         };
         (priority = $priority:tt, fn = $fn:ident) => {
+            $crate::__keep_alive!();
             $crate::__support::in_section!(
                 #[in_section(unsafe, type = mutable, name = _CTOR0_ISIZE_FN)]
                 const _: $crate::collect::Constructor = $crate::collect::Constructor {
@@ -176,6 +211,7 @@ pub mod collect {
             );
         };
         (priority = $priority:tt, fn = (array $array:ident)) => {
+            $crate::__keep_alive!();
             $crate::__support::in_section!(
                 #[in_section(unsafe, type = mutable, name = _CTOR0_ISIZE_FN)]
                 const _: [$crate::collect::Constructor; if $array.len() == 0 { 1 } else { $array.len() }] = {
